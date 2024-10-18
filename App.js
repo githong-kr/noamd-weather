1ce45ca857721261435e77a286af5ad0eb4df7e0
githong <githong@kakao.com>
2024-10-18 20:54:57 +0900

    what

--- App.js
+++ App.js
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Dimensions, ActivityIndicator } from 'react-native'
+import * as Location from 'expo-location'
+import React, { useState, useEffect } from 'react'
+
+const SCREEN_WIDTH = Dimensions.get('window').width
+const API_KEY = 'c075d9c4379bbde28d02ee75d3a31218'
 
 export default function App() {
+  const [city, setCity] = useState('Loading...')
+  const [days, setDays] = useState([])
+  const [ok, setOk] = useState()
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync()
+    if (!granted) {
+      setOk(false)
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 })
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false })
+    setCity(location[0].region)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    const json = await response.json()
+    setDays(json.list)
+  }
+
+  useEffect(() => {
+    getWeather()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="blue"
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View
+              key={index}
+              style={styles.day}
+            >
+              <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+              <Text style={styles.desc}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+              <Text style={styles.tinyText}>{day.dt_txt}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'tomato',
   },
-});
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: '500',
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  desc: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+  tinyText: {
+    fontSize: 20,
+  },
+})
